426718cb08d0bbe806b9ff48d09f3fbbb0cfd549
FuhrerTaco <36234256+FuhrerTaco@users.noreply.github.com>
2018-02-07 09:40:05 -0500

    Uploaded all current files.
    added all the current files relevant to the program.

---
+++ u1_01_Nolan/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿/***
+ * Nolan Meehan Feb 6
+ * 2018
+ * u1_01_Nolan
+ * Displays hello world. My "first" program.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace u1_01_Nolan
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHelloWorld_Click(object sender, RoutedEventArgs e)
+        {
+            lblHelloWorld.Content = "Hello World,my name is Nolan!";
+            btnHelloWorld.Visibility = Visibility.Hidden;
+        }
+
+        private void lblHelloWorld_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            lblHelloWorld.Content = "DON'T TOUCH ME!";
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
